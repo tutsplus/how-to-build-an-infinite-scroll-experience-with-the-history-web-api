c8aefde8ab73bbb3b62389bc4057fcc8e2b4d125
Thoriq Firdaus <tfirdaus@outlook.com>
2016-05-15 20:19:05 +0700

    code alignment
    - refine fn names,
    - improve formatting with jscs,
    - add placeholder fn

--- assets/js/src/script.jquery.js
+++ assets/js/src/script.jquery.js
@@ -6,39 +6,39 @@
  *  Made by Zeno Rocha
  *  Under MIT License
  */
-// the semi-colon before function invocation is a safety net against concatenated
+
+// The semi-colon before function invocation is a safety net against concatenated
 // scripts and/or other plugins which may not be closed properly.
 ;( function( $, window, document, undefined ) {
 
 	"use strict";
 
-		// undefined is used here as the undefined global variable in ECMAScript 3 is
+		// Undefined is used here as the undefined global variable in ECMAScript 3 is
 		// mutable (ie. it can be changed by someone else). undefined isn't really being
 		// passed in so we can ensure the value of it is truly undefined. In ES5, undefined
 		// can no longer be modified.
 
-		// window and document are passed through as local variable rather than global
+		// Window and document are passed through as local variable rather than global
 		// as this (slightly) quickens the resolution process and can be more efficiently
 		// minified (especially when both are regularly referenced in your plugin).
 
 		// Create the defaults once
-		var pluginName = "neverEnding";
-		var	baseURL = window.location.protocol + '//' + window.location.host + '/';
+		var pluginName = "keepScrolling";
 		var	defaults = {
-				article: "article",
+				article: ".article",
 				floor: "#footer",
 				data: null
 			};
 
-		var postFetching = false;
-		var postEnding = false;
+		var articleFetching = false;
+		var articleEnding = false;
 
 		// The actual plugin constructor
 		function Plugin ( element, options ) {
-			
+
 			this.element = element;
 
-			// jQuery has an extend method which merges the contents of two or
+			// JQuery has an extend method which merges the contents of two or
 			// more objects, storing the result in the first object. The first object
 			// is generally empty as we don't want to alter the default options for
 			// future instances of the plugin
@@ -51,6 +51,7 @@
 
 		// Avoid Plugin.prototype conflicts
 		$.extend( Plugin.prototype, {
+
 			init: function() {
 
 				// Place initialization logic here
@@ -65,21 +66,22 @@
 
 				this.fetch();
 				this.scroller();
+				this.placeholder();
 			},
 			debouncer: function( func, wait, immediate ) {
 
 				var timeout;
 
-				return function () {
+				return function() {
 
-					var context = this, 
+					var context = this,
 					args = arguments;
 
 					var later = function() {
 						timeout = null;
 						if ( !immediate ) {
 							func.apply( context, args );
-						};
+						}
 					};
 
 					var callNow = immediate && !timeout;
@@ -87,147 +89,171 @@
 					clearTimeout( timeout );
 					timeout = setTimeout( later, wait );
 
-					if ( callNow ) { 
+					if ( callNow ) {
 						func.apply( context, args );
 					}
-				}
+				};
 			},
-			getSiteAPI: function() {
+
+			getData: function() {
 				return this.settings.data;
 			},
+
 			getArticles: function() {
 				return $( this.element ).find( this.settings.article );
 			},
-			getPostURL: function( i ) {
-				return baseURL + this.getSiteAPI().posts[i].address + '.html';
-			},
-			getPrevArticle: function() {
+
+			getArticleURL: function( i ) {
 				
+				var	root = window.location.protocol + "//" + window.location.host + "/";
+				var address = this.getData()[ i ].address;
+
+				return root + address + ".html";
+			},
+
+			getNextArticle: function() {
+
 				var $last = this.getArticles().last();
 
-				var articlePrevAddr;
+				var articlePrevURL;
 				var articlePrevID = $last.data( "article" ).id - 1; // Previous ID
-				var siteAPI = this.getSiteAPI();
+				var articleData = this.getData();
 
-				for (var i = siteAPI.posts.length - 1; i >= 0; i--) {
-					if ( siteAPI.posts[ i ].id === articlePrevID ) {
-						articlePrevAddr = this.getPostURL( i ) ;
+				for ( var i = articleData.length - 1; i >= 0; i-- ) {
+					if ( articleData[ i ].id === articlePrevID ) {
+						articlePrevURL = this.getArticleURL( i ) ;
 					}
-				};
+				}
 
 				return {
-					id : articlePrevID,
-					url : articlePrevAddr
-				}
+					id: articlePrevID,
+					url: articlePrevURL
+				};
 			},
+
 			getSiteFloor: function() {
 
 				var floor = this.settings.floor;
 
-				return ( !floor.jquery ) ? $( floor ) : floor;
+					return ( !floor.jquery ) ? $( floor ) : floor;
 			},
+
+			placeholder: function() {
+
+				var tmplPlaceholder = document.getElementById( "tmpl-placeholder" );
+					tmplPlaceholder = tmplPlaceholder.innerHTML;
+					
+					$( main ).append( tmplPlaceholder );
+			},
+
 			visible: function( target ) {
 
 				if ( target instanceof jQuery ) {
-					target = target[0];
+					target = target[ 0 ];
 				}
 
 				var rect = target.getBoundingClientRect();
 
 				return rect.bottom > 0 &&
 					rect.right > 0 &&
-					rect.left < (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */ &&
-					rect.top < (window.innerHeight || document.documentElement.clientHeight) /*or $(window).height() */;
+					rect.left < ( window.innerWidth || document.documentElement.clientWidth ) /*or $(window).width() */ &&
+					rect.top < ( window.innerHeight || document.documentElement.clientHeight ) /*or $(window).height() */;
 			},
+
 			proceed: function() {
-		
-				if ( postFetching || postEnding || !this.visible( this.siteFloor ) ) {
+
+				if ( articleFetching || articleEnding || !this.visible( this.siteFloor ) ) {
 					return;
 				}
 
-				if ( this.getPrevArticle().id <= 0 ) {
-					postEnding = true;
+				if ( this.getNextArticle().id <= 0 ) {
+					articleEnding = true;
 					return;
 				}
 
 				return true;
 			},
+
 			fetch: function() {
 
 				if ( !this.proceed() ) {
 					return;
 				}
 
-				var main = this.element.id;
-				var articles = this.getArticles();
+				var main = this.element;
+				var $articleLast = this.getArticles().last();
 
 				$.ajax( {
-					url: this.getPrevArticle().url,
-					type: 'GET',
-					dataType: 'html',
-					beforeSend: function() {
-						postFetching = true;
+					url: this.getNextArticle().url,
+					type: "GET",
+					dataType: "html",
+					beforeSend: function() {					
+						$( main ).addClass( function() {
+							articleFetching = true;
+							return "fetching";
+						} );
 					}
 				} )
 				.done( function( res ) {
 
-					articles
-						.last()
+					$articleLast
 						.after( function() {
 							if ( !res ) {
 								return;
 							}
-							return $( res ).find( "#" + main ).html();
+							return $( res ).find( "#" + main.id ).html();
 						} );
 
-						Prism.highlightAll();
+						window.Prism.highlightAll();
 				} )
 				.always( function() {
-					postFetching = false;
+					$( main ).removeClass( function() {
+						articleFetching = false;
+						return "fetching";
+					} );
 				} );
 			},
+
 			history: function() {
 
-				if ( !History.enabled ) {
+				if ( !window.History.enabled ) {
 					return;
 				}
 
-				var self = this;
-
 				this.getArticles()
 					.each( function( index, article ) {
-					
+
 						var articleOffset = Math.floor( article.offsetTop - $( window ).scrollTop() );
 
 						if ( articleOffset > this.threshold ) {
 							return;
 						}
 
-						var apiPosts = this.getSiteAPI().posts;
+						var articleData = this.getData();
 						var articleID = $( article ).data( "article" ).id;
 						var articleIndex;
 
-						for (var i = apiPosts.length - 1; i >= 0; i--) {
-							if ( apiPosts[i].id === articleID ) {
+						for ( var i = articleData.length - 1; i >= 0; i-- ) {
+							if ( articleData[ i ].id === articleID ) {
 								articleIndex = i;
 							}
-						};
+						}
 
 						var articleFloor = Math.floor( ( article.clientHeight - ( this.threshold * 1.4 ) ) * -1 );
-						var articleURL = this.getPostURL( articleIndex );
+						var articleURL = this.getArticleURL( articleIndex );
 
 						if ( articleOffset > articleFloor && window.location.href !== articleURL ) {
-							History.pushState( null, this.getSiteAPI().posts[ articleIndex ].title, articleURL );
+							window.History.pushState( null, articleData[ articleIndex ].title, articleURL );
 						}
 
 					}.bind( this ) );
 			},
+
 			scroller: function() {
 
 				window.addEventListener( "scroll", this.debouncer( function() {
 					this.fetch();
 				}, 100 ).bind( this ), false );
-
 
 				window.addEventListener( "scroll", function() {
 					this.history();
@@ -238,10 +264,10 @@
 		// A really lightweight plugin wrapper around the constructor,
 		// preventing against multiple instantiations
 		$.fn[ pluginName ] = function( options ) {
+
 			return this.each( function() {
 				if ( !$.data( this, "plugin_" + pluginName ) ) {
-					$.data( this, "plugin_" +
-						pluginName, new Plugin( this, options ) );
+					$.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
 				}
 			} );
 		};
